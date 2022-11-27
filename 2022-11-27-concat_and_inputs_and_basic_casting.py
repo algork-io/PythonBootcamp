35ad63228b8709661f5375ed8b9a88394482a108
Sheikh Abdul Manan <sammanan4@gmail.com>
2022-11-27 20:57:03 +0530

    concat, input, casting

---
+++ 2022-11-27-concat_and_inputs_and_basic_casting.py
@@ -0,0 +1,50 @@
+# associativity & precedence
+equation = (5+4)*2
+# print(equation)
+
+
+# strings
+# + (concat), *(concat using mult)
+first_name="John"
+separator="-"*10
+last_name="Khan"
+full_name=first_name + separator + last_name
+# print(first_name)
+# print(last_name)
+# print(full_name)
+
+
+
+# taking input from user
+# full_name = input("Please enter full name: ")
+result_prefix = "Your full name is:"
+# s1 = "This is first"
+# s2 = "second"
+# s3 = s1+s2
+# print(s1+s2)
+# print("This is first")
+# print("second")
+# print("This is first", "second")
+# print(result_prefix, full_name)
+# print(type(full_name))
+
+# age = input("Please enter your age: ")
+# print("Your age is :",age)
+# print(type(age))
+
+
+
+# casting/type conversion
+# number_of_eng_books = input("Please enter number of English books: ")
+# number_of_urdu_books = input("Please enter number of Urdu books: ")
+# eng_books = int(number_of_eng_books)
+# urdu_books = int(number_of_urdu_books)
+# print(number_of_eng_books + number_of_urdu_books)
+# print(eng_books + urdu_books)
+
+
+# Excercise:
+# Take 5 variables from input
+# Perform addition between them
+# Perform multiplication between them
+# Add the first 2, add the second 2, multiply them, then subtract the last number from them
