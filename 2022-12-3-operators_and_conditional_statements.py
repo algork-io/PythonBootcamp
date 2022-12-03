227a905c8423e5ab35f00443081965e4176476ad
Syed Farhan <45279189+saeedfarhan@users.noreply.github.com>
2022-12-03 21:01:17 +0530

    Add files via upload

---
+++ 2022-12-3-operators_and_conditional_statements.py
@@ -0,0 +1,114 @@
+# Type casting
+type_str = 'hello world'
+type_int = 109
+type_float = 10.5
+type_bool = True
+
+
+a = 10
+converted_a = str(a)
+converted_a_float = float(a)
+# print(converted_a_float)
+# print(type(converted_a))
+
+b = 15.9
+converted_b = str(b)
+converted_b_int = int(b)
+# print(converted_b_int)
+
+
+c = False
+converted_c = int(c)
+converted_c_str = str(c)
+# print(converted_c_str)
+
+
+# Arthmetic operators
+# +,-,*...
+
+# Assignment operator
+# =,+=,-=,*=.....
+
+# = (assigns value on the rhs to the lhs)
+
+# a = 10
+# b = 5
+
+# a = b
+# print(a)
+
+#+= () 
+# a = a+10    =>   	a+=10
+# a = a+15  =>  	a+=15
+# a=a-12 	=>   	a-=12
+
+
+
+# Comparison operator
+
+# == -> (is equal to)
+# != => (not equal to)
+# >, < => 	(greater than opr less than)
+# >=, <= (greater or eqaul to | less or equal to)
+
+
+
+# a = 11
+# b = 10
+
+result = a == b
+# print(result)
+
+result = a != b
+# print(result)
+
+result = a>b
+# print(result)
+
+result = a>=b
+# print(result)
+
+
+
+
+# Conditional statements
+
+
+#  if statement
+var_a = int(input('enter first number')) 
+var_b = int(input('enter second number')) 
+# var_c = int(input('enter third number'))
+
+
+if var_a > var_b:
+	print('var_a is greater')
+	print('test statement')
+
+
+if var_a < var_b:
+	print('var_b is greater')
+
+# take 2 inputs from user and print 'success' if var_a is greater else print 'failure' 
+# if else statement
+
+if var_a > var_b:
+	print('success')
+
+else:
+	print('failure')
+
+	print('jhsadg')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
