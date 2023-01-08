498afa1584a858b46a29eb2411d96ca955ec424c
Syed Farhan <45279189+saeedfarhan@users.noreply.github.com>
2023-01-08 20:48:42 +0530

    Create 2023-8-1_file_handling.py

---
+++ 2023-8-1_file_handling.py
@@ -0,0 +1,107 @@
+# Create a dictionary from string, where keys are the index
+# of the characters (starting from 0) and values are the characters at the indices
+
+str1 = "Hello"
+resultDict = {}
+
+# for i in range(len(str1)):
+# 	resultDict[i] = str1[i]
+
+# print(resultDict)
+
+
+# for idx, i in enumerate(str1):
+# 	resultDict[idx] = i
+
+# print(resultDict)
+
+
+# converting 2 lists into a dict
+
+# l1 = ['key1','key2','key3']
+# l2 = ['val1','val2','val3']
+
+
+# newDict = {}
+
+# for i in range(len(l1)):
+# 	newDict[l1[i]] = l2[i]
+
+# print(newDict)
+
+
+# newDict = dict(zip(l1,l2))
+# print(newDict)
+
+
+
+
+# File handling
+
+# f = open('dummy.txt','r')
+# txt = f.readlines()
+# print(txt)
+# f.close()
+
+
+# f = open('dummy.txt','r+')
+# f.write('new hello world 3')
+# f.close()
+
+
+# with open('dummy.txt','r') as file:
+# 	print(file.read())
+
+
+
+
+# Working with packages
+
+
+# import datetime
+# from datetime import datetime as dt
+
+# import maths
+# from maths import add, sub
+
+# res = add(5,6)
+# res2 = sub(10,5)
+# print(res,res2)
+
+
+
+# res = 10
+
+
+
+
+# def add(a,b):
+# 	res = a+b
+# 	return res
+
+# def test():
+# 	# res = 209
+# 	global res
+# 	res+=1
+# 	print(res)
+
+# test()
+# print(res)
+
+
+
+
+# Find the count of each item in a list using dict where key will be the value in list and value will be the count its occurrences
+# Create a dict from list containing odd and even numbers {odd: [], even: [] }
+
+
+# Bubble sort
+# Selection sort
+# Palindrome
+
+
+
+
+# l1 = [1,2,3,4,5]
+# print(l1[::-1])
+
